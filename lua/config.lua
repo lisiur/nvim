1fde68189a1555b2e5d46931bb249ea59f2893e7
lisiur <lisiurday@gmail.com>
2022-09-25 23:52:18 +0800

    Refactor: User Config

--- lua/config.lua
+++ lua/config.lua
@@ -6,8 +6,14 @@ local function option(opt)
     or vars.is_windows and opt.windows
 end
 
-return {
+vim.g.user_config = {
+    theme = "dark", -- light / dark
+    prefer_light_theme = "latte", -- latte
+    prefer_dark_theme = "macchiato", -- frappe / macchiato / mocha
     neovide_font = "FiraCode\\ Nerd\\ Font:h15",
+    shell = option({
+        windows = "nu.exe",
+    }),
     python_host_prog = option({
         mac = "/usr/bin/python",
         linux = "/usr/bin/python",
@@ -20,5 +26,5 @@ return {
     }),
     sqlite_clib_path = option({
         windows = "C:\\Users\\lisiu\\.local\\bin\\sqlite3.dll"
-    })
+    }),
 }
